a03b49af24b572ed5640aeb069784abb4f8b2f05
sanyam <sanyamjainhbtu@gmail.com>
2026-06-22 19:39:31 +0530

    ake ui fast

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -31,7 +31,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           </div>
           {/* Mobile photo - shows only on mobile when 3D character is hidden */}
           <div className="mobile-photo">
-            <img src="/images/mypicnbg.png" alt="Sanyam Jain" />
+            <img src="/images/mypicbg.png" alt="Sanyam Jain" />
           </div>
         </div>
         {children}
